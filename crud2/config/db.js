9ed16cb7ea0d7aec14f89f46ffeefa0026d982bd
Devendradhote001 <ddhote780@gmail.com>
2026-01-21 17:14:22 +0530

    all crud operations

---
+++ crud2/config/db.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    let res = await mongoose.connect("mongodb://0.0.0.0/crud2");
+    if (res) {
+      console.log("mongodb Connected");
+    }
+  } catch (error) {
+    console.log("error while connectin db", error);
+  }
+};
+
+module.exports = connectDB;
